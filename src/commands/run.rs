12a243796cd403fcef30772acc24227374c04c39
Ian Wijma <ian@wij.ma>
2024-04-05 00:03:11 +1100

    Added npm, yarn, pnpm and composer task_engines. Added configTask parsers for the newly added task_engines. Added examples for the new task_engines.

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -4,6 +4,7 @@ use std::fs::{canonicalize, read_to_string};
 use std::path::{Path, PathBuf};
 use clap::Args;
 use glob::glob;
+use globset::{Glob, GlobSetBuilder};
 use serde::Deserialize;
 
 #[derive(Args, Debug)]
@@ -15,28 +16,27 @@ pub struct Arguments {
 }
 
 pub fn run (arguments: &Arguments) -> Result<(), String> {
-    let Arguments { entry, task_name: _task_name } = arguments;
+    let Arguments { entry, task_name } = arguments;
 
     // Resolve the entry path
     let entry_config_path: PathBuf = resolve_config_path(entry)?;
 
     // Discover all config paths
-    let config_paths: Vec<PathBuf> = discover_config_paths(entry_config_path)?;
+    let config_paths: Vec<PathBuf> = discover_config_paths(&entry_config_path)?;
 
     // Parse config file content
     let config_files: Vec<ConfigFile> = read_config_files(config_paths)?;
 
     // Parse config files
     let configs: Vec<Config> = parse_config_files(config_files)?;
-    println!("configs: {:?}", configs);
 
     // Resolve dependencies based on the directory structure
     // (In the future this will be configurable based on a dependency config field)
-    let config_structure: ConfigStructure = resolve_config_structure(configs)?;
-    println!("config_structure: {:?}", config_structure);
+    let config_structure: ConfigStructure = resolve_config_structure(&entry_config_path, configs)?;
 
     // Gather the tasks from the config
-    // let task_structure: TaskStructure = resolve_task_structure(config_structure, task_name);
+    let task_structure: TaskStructure = resolve_task_structure(config_structure, task_name);
+    eprintln!("task_structure = {:?}", task_structure);
 
     // Run the commands, one by one
     // > In the future this is configurable on the rask level and maybe on the config file level
@@ -47,22 +47,65 @@ pub fn run (arguments: &Arguments) -> Result<(), String> {
 }
 
 #[derive(Debug, Clone)]
-struct ConfigStructure {
-    config: Config,
-    child: HashMap<String, ConfigStructure>
+struct TaskStructure {
+
 }
 
-fn resolve_config_structure(_configs: Vec<Config>) -> Result<ConfigStructure, String> {
-    let _path_map: HashMap<PathBuf, Config> = HashMap::new();
+fn resolve_task_structure(config_structure: ConfigStructure, task_name: &String) -> TaskStructure {
+    todo!()
+}
 
-    // TODO: Create a recursive method that creates the config structure based on the directories.
+#[derive(Debug, Clone)]
+struct ConfigStructure {
+    config: Config,
+    children: Vec<ConfigStructure>
+}
+
+fn resolve_config_structure(entry_config_path: &PathBuf, configs: Vec<Config>) -> Result<ConfigStructure, String> {
+    let mut path_map: HashMap<PathBuf, Config> = HashMap::new();
+
+    for config in configs {
+        path_map.insert(config.clone().path, config);
+    }
+
+    let config_structure: ConfigStructure = construct_config_structure(entry_config_path, &path_map)?;
+
+    Ok(config_structure)
+}
+
+fn construct_config_structure(config_path: &PathBuf, config_path_map: &HashMap<PathBuf, Config>) -> Result<ConfigStructure, String> {
+    let config = config_path_map.get(config_path).ok_or("Unknown config path")?;
+
+    let paths: Vec<PathBuf> = config_path_map.keys().cloned().collect();
+    let Config { directories, .. } = config;
+    let config_directory: &Path = config_path.parent().unwrap();
+    let mut child_paths: Vec<PathBuf> = vec![];
+
+    for directory in directories {
+        let path_pattern: PathBuf = get_config_glob_pattern(config_directory, &directory);
+
+        // TODO: Maybe abstract?
+        let pattern = match Glob::new(path_pattern.to_str().unwrap()) {
+            Ok(pattern) => pattern,
+            Err(err) => return Err(format!("Failed to create glob pattern: {:?}", err)),
+        };
+        let mut builder = GlobSetBuilder::new();
+        builder.add(pattern);
+        let glob_set = builder.build().unwrap();
+
+        for path in &paths {
+            if glob_set.is_match(path) {
+                child_paths.push(path.to_path_buf());
+            }
+        }
+    }
+
     let config_structure = ConfigStructure {
-        config: Config{
-            name: "".to_string(),
-            tasks: Default::default(),
-            path: Default::default(),
-        },
-        child: HashMap::new()
+        config: config.clone(),
+        children: child_paths
+            .iter()
+            .map(|path| construct_config_structure(path, config_path_map).unwrap())
+            .collect()
     };
 
     Ok(config_structure)
@@ -81,13 +124,31 @@ struct ConfigTask {
 }
 
 type ConfigTasks = HashMap<String, ConfigTask>;
+type ConfigDirectories = Vec<String>;
+
+#[derive(Debug, Clone, Default, Deserialize)]
+enum TaskEngine {
+    #[serde(rename = "composer")]
+    COMPOSER,
+    #[serde(rename = "npm")]
+    NPM,
+    #[serde(rename = "yarn")]
+    YARN,
+    #[serde(rename = "pnpm")]
+    PNPM,
+    #[serde(rename = "none")]
+    #[default]
+    NONE
+}
 
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
 struct Config {
     name: String,
+    task_engine: TaskEngine,
     tasks: HashMap<String, ConfigTask>,
     path: PathBuf,
+    directories: ConfigDirectories,
 }
 
 fn parse_config_files(config_files: Vec<ConfigFile>) -> Result<Vec<Config>, String> {
@@ -102,13 +163,74 @@ fn parse_config_files(config_files: Vec<ConfigFile>) -> Result<Vec<Config>, Stri
 }
 
 fn parse_config_file(config_file: ConfigFile) -> Result<Config, String> {
-    let ConfigFile { name, tasks: config_file_tasks, _file_path: path, .. } = config_file;
+    let ConfigFile { name, tasks: config_file_tasks, _file_path: file_path, _dir_path: dir_path, directories, task_engine } = config_file;
 
-    let tasks = parse_config_tasks(config_file_tasks)?;
+    let tasks: ConfigTasks = match task_engine {
+        TaskEngine::COMPOSER => parse_composer_tasks(dir_path)?,
+        TaskEngine::NPM => parse_node_tasks(dir_path, "npm")?,
+        TaskEngine::YARN => parse_node_tasks(dir_path, "yarn")?,
+        TaskEngine::PNPM => parse_node_tasks(dir_path, "pnmp")?,
+        TaskEngine::NONE => parse_config_tasks(config_file_tasks)?,
+    };
 
-    let config: Config = Config {name, tasks, path};
+    let config: Config = Config { name, tasks, path: file_path, directories, task_engine };
 
     Ok(config)
+}
+
+#[derive(Debug, Clone, Deserialize, Default)]
+struct PackageJsonFile {
+    #[serde(default)]
+    scripts: HashMap<String, String>,
+}
+
+fn parse_node_tasks(dir_path: PathBuf, prefix: &str) -> Result<ConfigTasks, String> {
+    let mut file_path = dir_path.clone();
+    file_path.push("package.json");
+    let content = read_file_content(file_path)?;
+
+    let package_json: PackageJsonFile = serde_json::from_str(&content).expect(format!("Failed to package.json from \"{:?}\"", dir_path).as_str());
+
+    let mut config_tasks: ConfigTasks = HashMap::new();
+    for key in package_json.scripts.keys() {
+        config_tasks.insert(key.clone(), ConfigTask {
+            task_type: TaskType::SHELL,
+            content: format!("{:?} run {:?}", prefix, key)
+        });
+    }
+
+    Ok(config_tasks)
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+enum ComposerJsonScriptValue {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+#[derive(Debug, Clone, Deserialize, Default)]
+struct ComposerJsonFile {
+    #[serde(default)]
+    scripts: HashMap<String, ComposerJsonScriptValue>,
+}
+
+fn parse_composer_tasks(dir_path: PathBuf) -> Result<ConfigTasks, String> {
+    let mut file_path = dir_path.clone();
+    file_path.push("composer.json");
+    let content = read_file_content(file_path)?;
+
+    let package_json: ComposerJsonFile = serde_json::from_str(&content).expect(format!("Failed to composer.json from \"{:?}\"", dir_path).as_str());
+
+    let mut config_tasks: ConfigTasks = HashMap::new();
+    for key in package_json.scripts.keys() {
+        config_tasks.insert(key.clone(), ConfigTask {
+            task_type: TaskType::SHELL,
+            content: format!("composer run {:?}", key)
+        });
+    }
+
+    Ok(config_tasks)
 }
 
 fn parse_config_tasks(tasks: ConfigFileTasks) -> Result<ConfigTasks, String> {
@@ -137,7 +259,7 @@ fn read_config_files(paths: Vec<PathBuf>) -> Result<Vec<ConfigFile>, String> {
     Ok(configs_files)
 }
 
-fn discover_config_paths(path: PathBuf) -> Result<Vec<PathBuf>, String> {
+fn discover_config_paths(path: &PathBuf) -> Result<Vec<PathBuf>, String> {
     let mut found_config_paths: Vec<PathBuf> = vec![path.clone()];
 
     // Read config
@@ -148,11 +270,7 @@ fn discover_config_paths(path: PathBuf) -> Result<Vec<PathBuf>, String> {
         // Extract directories
         let config_directory = _file_path.parent().ok_or("Failed to get parent directory")?;
         for directory in directories {
-            let mut pattern: PathBuf = config_directory.to_path_buf();
-            pattern.push(&directory);
-            if !pattern.ends_with(".yaml") {
-                pattern.push("rask.yaml");
-            }
+            let pattern = get_config_glob_pattern(config_directory, &directory);
 
             // Find config files based on the pattern in the directories value
             let pattern_string: &str = pattern.to_str().unwrap();
@@ -171,6 +289,17 @@ fn discover_config_paths(path: PathBuf) -> Result<Vec<PathBuf>, String> {
     Ok(found_config_paths)
 }
 
+fn get_config_glob_pattern(root_path: &Path, glob_pattern: &String) -> PathBuf {
+    let mut pattern: PathBuf = root_path.to_path_buf();
+
+    pattern.push(glob_pattern);
+    if !pattern.ends_with(".yaml") {
+        pattern.push("rask.yaml");
+    }
+
+    pattern
+}
+
 fn read_file_content (path: PathBuf) -> Result<String, String> {
     match read_to_string(path) {
         Ok(content) => Ok(content),
@@ -184,11 +313,15 @@ type ConfigFileTasks = HashMap<String, String>;
 struct ConfigFile {
     name: String,
     #[serde(default)]
-    directories: Vec<String>,
+    task_engine: TaskEngine,
+    #[serde(default)]
+    directories: ConfigDirectories,
     #[serde(default)]
     tasks: ConfigFileTasks,
     #[serde(default)]
     _file_path: PathBuf,
+    #[serde(default)]
+    _dir_path: PathBuf,
 }
 
 fn read_config_file(path: PathBuf) -> Result<ConfigFile, String> {
@@ -197,6 +330,7 @@ fn read_config_file(path: PathBuf) -> Result<ConfigFile, String> {
     let mut config_file: ConfigFile = serde_yaml::from_str(&content).expect(format!("Failed to parse YAML from \"{:?}\"", path).as_str());
 
     config_file._file_path = path.clone();
+    config_file._dir_path = path.parent().unwrap().to_path_buf();
 
     Ok(config_file)
 }
